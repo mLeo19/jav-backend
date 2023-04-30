d7924adedff0a6703bbc9087d7599a3aa1cfb710
Leonardo <106184501+mLeo19@users.noreply.github.com>
2023-04-30 16:40:39 -0400

    added home page schema

---
+++ schemas/page.js
@@ -0,0 +1,44 @@
+export default {
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+      name: 'about_us_content',
+      title: 'About Us Content',
+      type: 'array',
+      of: [
+        { type: 'block' }
+      ]
+    },
+    {
+      name: 'about_us_image',
+      title: 'About Us Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'services_content',
+      title: 'Services Content',
+      type: 'array',
+      of: [
+        { type: 'block' }
+      ]
+    },
+    {
+      name: 'gallery_content',
+      title: 'Gallery Content',
+      type: 'array',
+      of: [
+        { type: 'block' }
+      ]
+    }
+  ]
+}
